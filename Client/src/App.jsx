2a4c7d4a1707dce235c4a9832d1a239d159c0c47
CelestinFernandes <celestin.fernandes21@st.niituniversity.in>
2024-11-23 17:00:14 +0530

    App.jsx update

--- Client/src/App.jsx
+++ Client/src/App.jsx
@@ -1,21 +1,25 @@
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
-import Home from './Pages/Home';
-import Upload from './Pages/Upload';
-import History from './Pages/History';
-import SignIn from './Pages/SignIn';
-import SignUp from './Pages/SignUp';
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Home from "./Pages/Home";
+import Upload from "./Pages/Upload";
+import History from "./Pages/History";
+import SignIn from "./Pages/SignIn";
+import SignUp from "./Pages/SignUp";
+import Footer from "./Components/Footer";
+import Header from "./Components/Header";
+import Toggle from "./Components/Toggle";
 
 export default function App() {
   return (
-  <BrowserRouter>
-    <Routes>
-      <Route path="/" element={<Home />} />
-      <Route path="/upload" element={<Upload />} />
-      <Route path="/history" element={<History />} />
-      <Route path="/signin" element={<SignIn />} />
-      <Route path="/signup" element={<SignUp />} />
-    </Routes>
-  </BrowserRouter>
-  )
-
+    <BrowserRouter>
+      <Header />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/upload" element={<Upload />} />
+        <Route path="/history" element={<History />} />
+        <Route path="/signin" element={<SignIn />} />
+        <Route path="/signup" element={<SignUp />} />
+      </Routes>
+      <Footer />
+    </BrowserRouter>
+  );
 }
