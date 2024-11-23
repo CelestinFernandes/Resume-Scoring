58b4f5bac3d5506b0ff22b26a98ddf8221eb8ca0
CelestinFernandes <celestin.fernandes21@st.niituniversity.in>
2024-11-23 10:52:46 +0530

    Basic Pages

--- Client/src/App.jsx
+++ Client/src/App.jsx
@@ -1,7 +1,21 @@
-import React from 'react'
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import Home from './Pages/Home';
+import Upload from './Pages/Upload';
+import History from './Pages/History';
+import SignIn from './Pages/SignIn';
+import SignUp from './Pages/SignUp';
 
 export default function App() {
   return (
-    <h1 className='text-red-500'>App</h1>
+  <BrowserRouter>
+    <Routes>
+      <Route path="/" element={<Home />} />
+      <Route path="/upload" element={<Upload />} />
+      <Route path="/history" element={<History />} />
+      <Route path="/signin" element={<SignIn />} />
+      <Route path="/signup" element={<SignUp />} />
+    </Routes>
+  </BrowserRouter>
   )
+
 }
